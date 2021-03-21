331a978fd1aaf9871e6791a7d159d756f85bab75
tommyrharper <thomasrobertharper@gmail.com>
2021-03-21 18:41:42 +0000

    Add react to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-(() => {
-  console.log('webpack worked')
-})()
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  React.createElement('div', null, `Hello React`),
+  document.getElementById('app')
+)
